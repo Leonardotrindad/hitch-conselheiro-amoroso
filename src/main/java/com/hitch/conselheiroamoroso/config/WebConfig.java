f145a0318ca5bd63d6f3ad1896e7e72a22b29afa
liv-inn <liviaavelino2002@gmail.com>
2025-11-17 17:46:47 -0300

    refactor(main): Remove frontend code from main branch

--- src/main/java/com/hitch/conselheiroamoroso/config/WebConfig.java
+++ src/main/java/com/hitch/conselheiroamoroso/config/WebConfig.java
@@ -11,9 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
